a961ad3b193d4f0c7f6059312fef7b170dbc0fdb
Kedar Abhyankar <krabhyankar@gmail.com>
2021-01-14 14:48:29 -0500

    PrintGradingResults finished as well as accessory methods. Working on saveGradingResults next.

--- src/main/java/edu/purdue/cs/percolator/VocareumFormatter.java
+++ src/main/java/edu/purdue/cs/percolator/VocareumFormatter.java
@@ -1,11 +1,42 @@
 package edu.purdue.cs.percolator;
 
 import com.github.tkutcher.jgrade.Grader;
+import com.github.tkutcher.jgrade.gradedtest.GradedTestResult;
+
+import java.util.List;
 
 class VocareumFormatter implements OutputFormatter {
 
+    //rubric name, number,
+    @Override
+    public void printGradingResults(Grader grader) {
+        List<GradedTestResult> gradedRes = grader.getGradedTestResults();
+        int numTestsFailed = (int) gradedRes.stream().filter(r -> !r.passed()).count();
+        if (numTestsFailed == 0) {
+            System.out.println("ALL TESTS PASSED!");
+            return;
+        }
+        System.out.println("TESTS FAILED! SEE BELOW FOR REPORT");
+        System.out.println("===========================================");
+        gradedRes.forEach(res -> {
+                if (!res.passed()) {
+                    System.out.println(formatGradedItem(res));
+                }
+            }
+        );
+    }
+
     public void saveGradingResults(Grader grader) {
         // TODO
+    }
+
+    private String formatGradedItem(GradedTestResult gtr) {
+        if (gtr.passed()) {
+            return gtr.getName() + ": TEST PASSED!";
+        } else {
+            return gtr.getName() + ": " + gtr.getScore() + " / " +
+                gtr.getPoints() + "TEST FAILED. See result below.";
+        }
     }
 
 }
